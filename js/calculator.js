26f02a74f629f064bb8e352c55e3f2c264b1a62d
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-03-15 11:01:34 -0500

    Write operate function

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,26 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+  switch(operator) {
+    case 'add':
+      return add(firstNumber, secondNumber);
+      break;
+    case 'subtract':
+      return subtract(firstNumber, secondNumber);
+      break;
+    case 'multiply':
+      return multiply(firstNumber, secondNumber);
+      break;
+    case 'divide':
+      return divide(firstNumber, secondNumber);
+      break;
+    default:
+      return 'Invalid operator!'; 
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
