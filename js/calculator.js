0a50a4b80695cd395a734d11f2ed9e5a52abff0e
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-03-18 15:56:57 -0500

    Add basic functionality to the buttons

--- js/calculator.js
+++ js/calculator.js
@@ -2,18 +2,52 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+let display = document.querySelector('.display');
+
+document.querySelectorAll('.button').forEach(function(button) {
+  button.addEventListener('click', function() {
+    let buttonValue = this.textContent;
+
+    switch(buttonValue) {
+      case 'C':
+        display.textContent = '0';
+        break;
+      case '=':
+        secondNumber = parseInt(display.textContent);
+        display.textContent = operate(firstNumber, secondNumber, operator);
+        break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        firstNumber = parseInt(display.textContent);
+        operator = buttonValue;
+        display.textContent = '0';
+        break;
+      default:
+        if (display.textContent === '0') {
+          display.textContent = buttonValue;
+        }
+        else {
+          display.textContent += buttonValue;
+        }
+        break;
+    }
+  })
+})
+
 function operate(firstNumber, secondNumber, operator) {
   switch(operator) {
-    case 'add':
+    case '+':
       return add(firstNumber, secondNumber);
       break;
-    case 'subtract':
+    case '-':
       return subtract(firstNumber, secondNumber);
       break;
-    case 'multiply':
+    case '*':
       return multiply(firstNumber, secondNumber);
       break;
-    case 'divide':
+    case '/':
       return divide(firstNumber, secondNumber);
       break;
     default:
